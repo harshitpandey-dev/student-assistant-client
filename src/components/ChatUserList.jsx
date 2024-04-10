a8525eec263ccf0625aff74040d4b625b31bc3ab
harshit1142 <harshitpandey98765@gmail.com>
2024-04-11 00:22:01 +0530

    Fix:CSS

--- src/components/ChatUserList.jsx
+++ src/components/ChatUserList.jsx
@@ -8,7 +8,7 @@ export default function ChatUserList({list,userID,chatID}) {
     const active=(chatID===list._id);
 
     const lastMsg = list?.lastMessage?.content.length > 20 ? list?.lastMessage?.content.substring(0, 20)+"...." : list?.lastMessage?.content;
-  
+    
    
     return (
 
