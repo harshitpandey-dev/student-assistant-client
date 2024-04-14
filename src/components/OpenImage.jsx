ed628850873f4f39835fc2d0298e561d569082a2
harshit1142 <harshitpandey98765@gmail.com>
2024-04-14 16:24:48 +0530

    Fix:responsive

--- src/components/OpenImage.jsx
+++ src/components/OpenImage.jsx
@@ -9,8 +9,8 @@ export default function OpenImage({handleClose,show,url,msg}) {
           <Modal show={show} onHide={handleClose}>
               <Modal.Header closeButton varient="danger" className='bg-dark'>
               </Modal.Header>
-              <Modal.Body className='bg-dark'>
-                 <img src={url}></img>
+              <Modal.Body className='bg-dark' >
+                 <img src={url} style={{width:"100%"}}></img>
               </Modal.Body>
               <Modal.Footer style={{ fontFamily: "'Gluten', sans-serif" }} className='bg-dark text-light'>
               {msg}
